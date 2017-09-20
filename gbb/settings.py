16903018a99afde27b5e5d0abf608310dc06af93
John Cooley <johnyc213@gmail.com>
2017-09-20 10:36:37 -0700

    static stuff

--- gbb/settings.py
+++ gbb/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    #os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(BASE_DIR, 'static')
 )
 
