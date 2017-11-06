2e90639d9a55e702f34dd3ffd5558b3d467793f4
John Cooley <johnyc213@gmail.com>
2017-11-05 21:48:09 -0800

    ssl redirect turned on

--- gbb/settings.py
+++ gbb/settings.py
@@ -132,3 +132,8 @@ STATIC_ROOT = 'static'
 SITE_ID = 1
 
 AWS_S3_HOST = 's3-us-west-2.amazonaws.com'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
